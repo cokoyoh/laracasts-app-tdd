3e7272799266b365d327ada59ab6fab999e1d5de
cokoyoh <charlesokoyoh@gmail.com>
2019-09-24 13:59:50 +0300

    Add utility first classes for the projects page

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <header class="flex items-center py-4">
-        <div class="flex justify-between items-center w-full">
+        <div class="flex justify-between items-end w-full">
             <h3 class="text-gray-700 text-sm font-normal">My Projects</h3>
             <a href="/projects/create" class="button">New Project</a>
         </div>
@@ -11,13 +11,7 @@
     <div class="lg:flex lg:flex-wrap -mx-3">
         @forelse($projects as $project)
             <div class="lg:w-1/3 px-3 pb-6">
-                <div class="bg-white p-5 rounded-lg shadow" style="height: 200px">
-                    <h3 class="font-normal text-xl py-4 -ml-5 border-l-4 border-blue-300 pl-4 mb-3 ">
-                        <a href="{!! $project->path() !!}" class="text-black no-underline">{!! $project->title !!}</a>
-                    </h3>
-
-                    <div class="text-gray-600">{!! Str::limit($project->description, 100) !!}</div>
-                </div>
+                @include('projects.partials.card')
             </div>
         @empty
             <div>No projects yet</div>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,7 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{!! $project->title !!}</h1>
-    <div>{!! $project->description !!}</div>
-    <a href="/projects">Go Back</a>
+    <header class="flex items-center py-4 mb-6">
+        <div class="flex justify-between items-end w-full">
+            <p class="text-gray-700 text-sm font-normal">
+                <a href="/projects" class="text-gray-700 text-sm font-normal no-underline">My Projects</a> / {!! $project->title !!}
+            </p>
+
+            <a href="/projects/create" class="button">New Project</a>
+        </div>
+    </header>
+
+    <main>
+       <div class="lg:flex -m-3">
+           <div class="lg:w-3/4 px-3 mb-6">
+               <div class="mb-8">
+                   <h3 class="text-lg text-gray-700 font-normal mb-3">Tasks</h3>
+
+                   <div class="card mb-3">Lorem ipsum.</div>
+                   <div class="card mb-3">Lorem ipsum.</div>
+                   <div class="card mb-3">Lorem ipsum.</div>
+                   <div class="card">Lorem ipsum.</div>
+
+                   {{-- Tasks--}}
+               </div>
+
+              <div>
+                  <h3 class="text-lg text-gray-700 font-normal mb-3">General Notes</h3>
+
+                  {{-- General Notes--}}
+                  <textarea class="card w-full" style="min-height: 200px">Lorem ipsum.</textarea>
+              </div>
+           </div>
+
+           <div class="lg:w-1/4 px-3 mt-8">
+               @include('projects.partials.card')
+           </div>
+
+       </div>
+    </main>
+
 @endsection
