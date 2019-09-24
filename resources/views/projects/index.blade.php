25424ed0aa3fd92363852c746254d14654f8517b
cokoyoh <charlesokoyoh@gmail.com>
2019-09-24 11:09:00 +0300

    Add utility first tailwind classes

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -5,12 +5,12 @@
         <a href="/projects/create">New Project</a>
     </div>
 
-    <div>
+    <div class="flex">
         @forelse($projects as $project)
-            <div>
-                <h3>{!! $project->title !!}</h3>
+            <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px">
+                <h3 class="font-normal text-xl py-4">{!! $project->title !!}</h3>
 
-                <div>{!! $project->description !!}</div>
+                <div class="text-gray-600">{!! Str::limit($project->description, 100) !!}</div>
             </div>
         @empty
             <div>No projects yet</div>
