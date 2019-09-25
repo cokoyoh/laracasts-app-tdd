eb4d198ce3973bf13089943616689cbc443262d3
cokoyoh <charlesokoyoh@gmail.com>
2019-09-25 10:25:52 +0300

    Style the create projects page

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,32 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    <form method="post" action="/projects">
-        @csrf
+    <div class="flex justify-center">
+        <div class="card w-1/2">
+            <form method="post" action="/projects">
+                @csrf
 
-        <h1 class="heading is-1">Create a Project</h1>
+                <h1 class="text-gray-700 mb-4 text-lg">Create a Project</h1>
 
-        <div class="field">
-            <label for="title" class="label">Title</label>
+                <div class="mb-4">
+                    <label for="title" class="text-gray-700 mb-6">Title</label>
 
-            <div class="control">
-                <input type="text" class="input" name="title" placeholder="Title">
-            </div>
+                    <div class="">
+                        <input type="text" class="w-full card" name="title" placeholder="Title">
+                    </div>
+                </div>
+
+                <div class="mb-4">
+                    <label for="description" class="text-gray-700 mb-4">Description</label>
+
+                    <div class="">
+                        <textarea class="w-full card" name="description" placeholder="Description" rows="5"> </textarea>
+                    </div>
+                </div>
+
+                <div class="">
+                    <div class="flex content-center ">
+                        <button type="submit" class="button mr-5">Create Project</button>
+                        <a href="/projects" class="py-2 text-gray-500">Cancel</a>
+                    </div>
+                </div>
+            </form>
         </div>
-
-        <div class="field">
-            <label for="description" class="label">Description</label>
-
-            <div class="control">
-                <textarea class="textarea" name="description" placeholder="Description"> </textarea>
-            </div>
-        </div>
-
-        <div class="field">
-            <div class="control">
-                <button type="submit" class="button is-link">Create Project</button>
-                <a href="/projects">Cancel</a>
-            </div>
-        </div>
-    </form>
+    </div>
 @endsection
