f7ae753862cdbe811201a56ae56a88b6521fae72
cokoyoh <charlesokoyoh@gmail.com>
2019-09-30 16:26:49 +0300

    An activity is associated with a user

--- app/RecordsActivityTrait.php
+++ app/RecordsActivityTrait.php
@@ -47,7 +47,8 @@ trait RecordsActivityTrait
         $this->activity()->create([
             'description' => $description,
             'changes' => $this->activityChanges(),
-            'project_id' => class_basename($this) == 'Project' ? $this->id : $this->project_id
+            'project_id' => class_basename($this) == 'Project' ? $this->id : $this->project_id,
+            'user_id' =>($this->project ?? $this)->owner->id,
         ]);
     }
 
