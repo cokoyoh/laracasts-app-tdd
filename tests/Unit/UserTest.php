6ee43daf56e35efbb453f6c41f53f4c7579511cd
cokoyoh <charlesokoyoh@gmail.com>
2019-09-30 22:17:56 +0300

    A user has access to projects they are invited to

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\User;
+use Facades\Tests\Setup\ProjectFactory;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -17,5 +18,27 @@ class UserTest extends TestCase
         $user = create(User::class);
 
         $this->assertInstanceOf(Collection::class, $user->projects);
+    }
+
+    /** @test */
+    public function a_user_has_accessible_projects()
+    {
+        $john = $this->signIn();
+
+        ProjectFactory::ownedBy($john)->create();
+
+        $this->assertCount(1, $john->accessibleProjects());
+
+        $sally = create(User::class);
+
+        $nick = create(User::class);
+
+        $project = tap(ProjectFactory::ownedBy($sally)->create())->invite($nick);
+
+        $this->assertCount(1, $john->accessibleProjects());
+
+        $project->invite($john);
+
+        $this->assertCount(2, $john->accessibleProjects());
     }
 }
